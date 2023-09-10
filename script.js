f3aa7539fd8aea37bacb1a9f309d397e4aaab4b1
Pietro C.S <136848602+pietrossantos@users.noreply.github.com>
2023-09-10 10:43:34 -0300

    adicionado os arquivos

---
+++ script.js
@@ -0,0 +1,93 @@
+
+let comida = "string";
+let number = 123454321;
+let right = true;
+
+console.log(comida, number, right);
+
+prompt('seja bem vindo')
+
+
+console.log("String -> Na sua essência, todos os dados do tipo string são textos, seja uma palavra, uma frase, caracteres... para serem considerados dados do tipo string as informações tem que estar envolvidas entre aspas, seja aspas duplas  ou aspas simples '' ");
+console.log("Number -> Dados do tipo númerico (numeros) no javascript não é necessário colocar aspas para números, apenas escrever o número e ele identifica automaticamente. identifica somente números inteiro ou números com ponto no caso 15.7, 7.5, 8.2");
+console.log("Boolean (Booleanos) -> Em sua essência são dados verdadeiros (true) ou falsos (false)");
+
+
+
+const compras = 16;
+
+if( compras <= 18) {
+    
+    console.log(true);
+    alert("olá, seja bem vindo ao nosso site");
+}
+else if(compras >= 19){
+    console.log(false)
+    alert("você não pode acessar essa página");
+}
+
+
+
+let calculator = 1;
+for(let calculator = 1; calculator <=100; calculator +=2) {
+
+console.log(calculator);
+
+}
+
+
+
+valor1 = 10;
+valor2 = 10;
+valor3 = 10;
+
+
+
+
+if (vfinal = valor1 + valor2 + valor3){
+console.log(valor1, "+", valor2, "+",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 - valor2 + valor3){
+    console.log(valor1, "-", valor2, "+",valor3, "=", vfinal);
+}
+
+if  (vfinal = valor1 - valor2 - valor3){
+      console.log(valor1, "-", valor2, "-",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 * valor2 * valor3){
+        console.log(valor1, "*", valor2, "*",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 * valor2 / valor3){
+    console.log(valor1, "*", valor2, "/",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 / valor2 / valor3){
+  console.log(valor1, "/", valor2, "/",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 / valor2 + valor3){
+  console.log(valor1, "/", valor2, "+",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 / valor2 - valor3){
+  console.log(valor1, "/", valor2, "-",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 - valor2 / valor3){
+  console.log(valor1, "-", valor2, "/",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 * valor2 - valor3){
+  console.log(valor1, "*", valor2, "-",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 + valor2 * valor3){
+  console.log(valor1, "+", valor2, "*",valor3, "=", vfinal);
+}
+
+if (vfinal = valor1 - valor2 * valor3){
+  console.log(valor1, "-", valor2, "*",valor3, "=", vfinal);
+}
