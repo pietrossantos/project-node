28aa1c89530940da284b2b09c4a09a6706a3201a
pietrossantos <=>
2023-10-15 17:57:04 -0300

    first commit

---
+++ yarn_react/src/Checklist.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Checklist.css'
+
+function Checklist() {
+
+  return (
+    <>
+     <section>
+      <h1>Shopping list</h1>
+      <input placeholder='products' />
+     </section>
+    </>
+  )
+}
+
+export default Checklist
---
+++ yarn_react/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Checklist.jsx'
+import './index.css'
+import Checklist from './Checklist.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Checklist />
+  </React.StrictMode>,
+)
